e804d64bc406dbb33c4999076ab3bbd479301bd0
abdulrahim40 <abdulrahim40@admin.sma.belajar.id>
2026-05-31 20:07:45 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,18 @@
-const CACHE_NAME = 'taskflow-cache-v1';
+const CACHE_NAME = 'taskflow-v1';
 const urlsToCache = [
   './',
   './index.html',
-  'https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,400;500;600;700&display=swap',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
+  './manifest.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map(key => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      })
-    ))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
